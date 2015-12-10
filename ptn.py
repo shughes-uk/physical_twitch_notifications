3e1887c94e328b9beab469a597307d0112e1364f
shughes <shughes.uk@gmail.com>
2015-12-10 00:25:33 -0600

    my greatest work..

--- ptn.py
+++ ptn.py
@@ -4,20 +4,26 @@ import time
 import sys
 import argparse
 import webcolors
-from devices import Hue, KankunSocket
+from devices import Hue, KankunSocket, BlinkyTape
 from yaml import load
 from pprint import pformat
 from twitchchat import twitch_chat
 from twitcher import twitcher
-
+import json
+import urllib
 logger = logging.getLogger("ptn")
+twitch_log = logging.getLogger('twitch')
+twitch_log.setLevel(logging.CRITICAL)
+twitchchat_log = logging.getLogger('twitch_chat')
+twitchchat_log.setLevel(logging.CRITICAL)
 
 
 class ptn(object):
 
     def __init__(self, test=False):
         logger.info("ptn starting up")
-        subs = ['on_follower', 'on_subscriber', 'on_start_streaming', 'on_stop_streaming', 'on_follower_count']
+        subs = ['on_follower', 'on_subscriber', 'on_start_streaming', 'on_stop_streaming', 'on_follower_count',
+                'on_subscriber_count']
         self.twitchchat = None
         self.twitcher = None
         self.test_mode = test
@@ -48,10 +54,10 @@ class ptn(object):
                 from twitch.api import v3 as twitch
                 featured_stream = twitch.streams.featured(limit=1)['featured'][0]
                 self.load_twitcher(featured_stream['stream']['channel']['name'])
-                self.load_twitchchat(config['twitch_username'], config['twitch_oauth'],
+                self.load_twitchchat(config['twitch_username'], config['twitch_chat_oauth'],
                                      featured_stream['stream']['channel']['name'])
             else:
-                self.load_twitchchat(config['twitch_username'], config['twitch_oauth'], config['twitch_channel'])
+                self.load_twitchchat(config['twitch_username'], config['twitch_chat_oauth'], config['twitch_channel'])
                 self.load_twitcher(config['twitch_channel'])
             self.load_devices(config['devices'])
             self.setup_subscriptions()
@@ -70,7 +76,7 @@ class ptn(object):
     def setup_subscriptions(self):
         if self.subscriptions['on_follower'] or self.subscriptions['on_follower_count']:
             self.twitcher.subscribe_new_follow(self.on_follower)
-        if self.subscriptions['on_subscriber']:
+        if self.subscriptions['on_subscriber'] or self.subscriptions['on_subscriber_count']:
             self.twitchchat.subscribeNewSubscriber(self.on_subscriber)
         if self.subscriptions['on_start_streaming']:
             self.twitcher.subscribe_streaming_start(self.started_streaming)
@@ -85,28 +91,53 @@ class ptn(object):
             count = f_config['times_to_flash']
             speed = f_config['flash_speed']
             device.flash(c1, c2, count, speed)
+        elif cfg['action'].keys()[0] == 'set_color':
+            f_config = cfg['action']['set_color']
+            c1 = webcolors.name_to_rgb(f_config['color'])
+            device.set_color(c1)
+        elif cfg['action'].keys()[0] == 'turn_on':
+            device.turn_on()
+        elif cfg['action'].keys()[0] == 'turn_off':
+            device.turn_off()
+        elif cfg['action'].keys()[0] == 'turn_on_timer':
+            f_config = cfg['action']['turn_on_timer']
+            duration = f_config['duration']
+            device.turn_on_timer(duration)
+        elif cfg['action'].keys()[0] == 'turn_off_timer':
+            f_config = cfg['action']['turn_off_timer']
+            duration = f_config['duration']
+            device.turn_off_timer(duration)
 
     def started_streaming(self, streamer_name):
         for device, cfg in self.subscriptions['on_start_streaming']:
+            logger.info("Triggered subscription by {0} for stream start".format(device))
             self.handle_action(device, cfg)
 
     def stopped_streaming(self, streamer_name):
-        logger.info(pformat(args))
-        logger.info('stopped streaming')
+        for device, cfg in self.subscriptions['on_stop_streaming']:
+            logger.info("Triggered subscription by {0} for stream stop".format(device))
+            self.handle_action(device, cfg)
 
     def on_follower(self, followerset, streamername, total):
         for device, cfg in self.subscriptions['on_follower_count']:
-            logger.info(pformat(cfg))
             if total >= cfg['count'] and not cfg['triggered']:
+                logger.info("Triggered subscription by {0} for follower count".format(device))
                 self.handle_action(device, cfg)
                 cfg['triggered'] = True
-                break
         for device, cfg in self.subscriptions['on_follower']:
+            logger.info("Triggered subscription by {0} for new follower".format(device))
             self.handle_action(device, cfg)
 
     def on_subscriber(self, channel, subscriber, months):
-        logger.info(pformat(args))
-        logger.info('new subscriber')
+        for device, cfg in self.subscriptions['on_subscriber_count']:
+            total = self.get_subscribercount(channel, self.config['twitch_subscriber_oauth'])
+            if total >= cfg['count'] and not cfg['triggered']:
+                logger.info("Triggered subscription by {0} for subscriber count".format(device))
+                self.handle_action(device, cfg)
+                cfg['triggered'] = True
+        for device, cfg in self.subscriptions['on_subscriber']:
+            logger.info("Triggered subscription by {0} for new subscriber".format(device))
+            self.handle_action(device, cfg)
 
     def load_devices(self, devicecfg):
         for devicename in devicecfg:
@@ -114,6 +145,13 @@ class ptn(object):
                 self.configure_kankun(devicecfg[devicename])
             elif devicecfg[devicename]['type'] == 'hue':
                 self.configure_phue(devicecfg[devicename])
+            elif devicecfg[devicename]['type'] == 'blinkytape':
+                self.configure_blinkytape(devicecfg[devicename])
+
+    def configure_blinkytape(self, bconfig):
+        blinkytape = BlinkyTape(bconfig['port'])
+        self.configure_subscriptions(blinkytape, bconfig['subscriptions'])
+        self.devices.append(blinkytape)
 
     def configure_kankun(self, kankuncfg):
         kankunsocket = KankunSocket(kankuncfg["ip"])
@@ -127,16 +165,19 @@ class ptn(object):
 
     def configure_subscriptions(self, device, subcfg):
         for subscription in subcfg:
-            if subscription == "on_subscriber":
-                self.subscriptions['on_subscriber'].append((device, subcfg[subscription]))
-            elif subscription == "on_follower":
-                self.subscriptions['on_follower'].append((device, subcfg[subscription]))
-            elif subscription == "on_start_streaming":
-                self.subscriptions['on_start_streaming'].append((device, subcfg[subscription]))
-            elif subscription == "on_stop_streaming":
-                self.subscriptions['on_stop_streaming'].append((device, subcfg[subscription]))
-            elif subscription == "on_follower_count":
-                self.subscriptions['on_follower_count'].append((device, subcfg[subscription]))
+            if subscription in self.subscriptions:
+                self.subscriptions[subscription].append((device, subcfg[subscription]))
+            else:
+                logger.warn("Unknown subscription option {0}".format(subscription))
+
+    def get_subscribercount(self, name, oauth):
+        url = "https://api.twitch.tv/kraken/channels/{0}/subscriptions?oauth_token={1}".format(name, oauth)
+        response = urllib.urlopen(url)
+        data = json.load(response)
+        if '_total' in data:
+            return data['_total']
+        else:
+            return 0
 
 # 'main'
 if __name__ == "__main__":
